21197c2755a20210f44002fbc7e5fa682a7b857a
Retail Hugo <hugo.retail@u-bordeaux.fr>
2024-12-11 15:40:53 +0100

    added search bar, small title animation

--- miuts/src/App.js
+++ miuts/src/App.js
@@ -1,20 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className="text-3xl font-bold underline">
-          Hello World!
+        <h1 className="text-3xl font-bold animate-bounce">
+          IUT Skills
         </h1>
+
+        <SearchBar />
       </header>
     </div>
   );
 }
 
-function Button() {
+function SearchBar() {
   return (
-    <button>Click me! Op</button>
+    <input 
+      type="text" 
+      placeholder="Search..." 
+      className="border rounded py-2 px-4 text-blue-800 my-4"  
+    />
   );
 }
