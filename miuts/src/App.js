d2cc02a8187984110e67e95be5f0af031494d8b4
Retail Hugo <hugo.retail@etu.u-bordeaux.fr>
2025-01-19 11:58:49 +0100

    imported test data; cleared some style

--- miuts/src/App.js
+++ miuts/src/App.js
@@ -1,20 +1,16 @@
 import "./App.css";
-import Title from './components/Title';
-import Navbar from './components/Navbar';
 import Searchbar from './components/SearchBar';
-import Footer from './components/Footer';
+import Title from './components/Title';
 
 export default function App() {
   return (
     <div className="App w-full">
-      <Navbar />
 
       <main className="min-h-screen flex flex-col items-center justify-center bg-gray-900 text-white">
         <Title />
         <Searchbar />
       </main>
 
-      <Footer />
     </div>
   );
 }
--- miuts/src/components/SearchBar.jsx
+++ miuts/src/components/SearchBar.jsx
@@ -4,12 +4,9 @@ export default function Searchbar() {
       <div className="relative flex items-stretch w-full">
         <input
           type="search"
-          className="flex-auto rounded-l border border-solid border-neutral-300 bg-transparent px-3 py-2 text-base text-neutral-700 placeholder-neutral-400 focus:border-blue-500 focus:outline-none focus:ring focus:ring-blue-500"
+          className="flex-auto text-white rounded border border-solid border-neutral-300 bg-transparent px-3 py-2 text-base text-neutral-700 placeholder-neutral-400 focus:border-blue-500 focus:outline-none focus:ring focus:ring-blue-500"
           placeholder="Search"
         />
-        <button className="bg-blue-600 text-white px-4 py-2 rounded-r hover:bg-blue-700 focus:outline-none focus:ring focus:ring-blue-500">
-          Search
-        </button>
       </div>
     </div>
   );
