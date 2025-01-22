7c0ac76b07383353c6b3cbe38892b855fcdd0d76
Retail Hugo <hugo.retail@u-bordeaux.fr>
2025-01-22 14:13:04 +0100

    added project description; details page items organization update

--- miuts/src/components/Details.jsx
+++ miuts/src/components/Details.jsx
@@ -5,22 +5,26 @@ export default function Details({ item }) {
 
   return (
     <div className="mt-4 p-4 bg-gray-800 border border-gray-700 rounded shadow-lg">
-      <h2 className="text-2xl font-bold mb-2">{item.Ressource}</h2>
-      {item.Nom && (
-        <p>
-          <strong>Nom :</strong> {item.Nom}
-        </p>
+      <h2 className="text-2xl font-bold mb-2">
+        {item.Ressource}
+        {item.Nom && (
+          <span>
+            {' - '}
+            {item.Nom}
+          </span>
+        )}
+        {item.Semestre && (
+          <span>
+            {' - '}
+            S{item.Semestre}
+          </span>
+        )}
+      </h2>
+
+      {item.IUT === "TRUE" && (
+        <p className="text-sm text-gray-400">(IUT)</p>
       )}
-      {item.Semestre && (
-        <p>
-          <strong>Semestre :</strong> {item.Semestre}
-        </p>
-      )}
-      {item.IUT && (
-        <p>
-          <strong>IUT :</strong> {item.IUT}
-        </p>
-      )}
+      
       {item.NomImage && (
         <img
           src={`/logo/${item.NomImage}.webp`}
